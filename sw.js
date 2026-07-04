48e53e8f5e6bd7606f313b5f25e5a97d0061dad6
fullmodel-star <c0787@csd.org.tw>
2026-07-04 12:20:57 +0800

    跟讀改逐字綠/紅標記+慢速鈕；開放應答改關鍵字回饋；兒童模式大字+諧音；sw v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='travel-en-v1';
+const C='travel-en-v2';
 const A=['index.html','manifest.json','icon-192.png','icon-512.png','icon-maskable-512.png','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))))});
